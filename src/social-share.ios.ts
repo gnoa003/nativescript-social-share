89434ca99c2adc984d2f23a0cb28e491f87466e3
Gus Noa <noa.gustavo@yahoo.com>
2019-04-22 23:24:21 -0400

    Exclude Activity Types
    Exclude activity types: UIActivityTypeMail, UIActivityTypeAssignToContact, UIActivityTypePrint, UIActivityTypePostToTwitter, UIActivityTypePostToWeibo

--- src/social-share.ios.ts
+++ src/social-share.ios.ts
@@ -1,32 +1,30 @@
 import { topmost } from "tns-core-modules/ui/frame";
 
 function share(thingsToShare) {
-  var activityController = UIActivityViewController.alloc()
-    .initWithActivityItemsApplicationActivities(thingsToShare, null);
+    var activityController = UIActivityViewController.alloc().initWithActivityItemsApplicationActivities(thingsToShare, null);
+    activityController.excludedActivityTypes = [UIActivityTypeMail, UIActivityTypeAssignToContact, UIActivityTypePrint, UIActivityTypePostToTwitter, UIActivityTypePostToWeibo];
 
-  var presentViewController = activityController.popoverPresentationController;
-  if (presentViewController) {
-    var page = topmost().currentPage;
-    if (page && page.ios.navigationItem.rightBarButtonItems &&
-      page.ios.navigationItem.rightBarButtonItems.count > 0) {
-      presentViewController.barButtonItem = page.ios.navigationItem.rightBarButtonItems[0];
-    } else {
-      presentViewController.sourceView = page.ios.view;
+    var presentViewController = activityController.popoverPresentationController;
+    if (presentViewController) {
+        var page = topmost().currentPage;
+        if (page && page.ios.navigationItem.rightBarButtonItems && page.ios.navigationItem.rightBarButtonItems.count > 0) {
+            presentViewController.barButtonItem = page.ios.navigationItem.rightBarButtonItems[0];
+        } else {
+            presentViewController.sourceView = page.ios.view;
+        }
     }
-  }
 
-  topmost().ios.controller
-    .presentViewControllerAnimatedCompletion(activityController, true, null);
+    topmost().ios.controller.presentViewControllerAnimatedCompletion(activityController, true, null);
 }
 
 export function shareImage(image) {
-  share([image]);
+    share([image]);
 }
 
 export function shareText(text) {
-  share([text]);
+    share([text]);
 }
 
 export function shareUrl(url, text) {
-  share([NSURL.URLWithString(url), text]);
+    share([NSURL.URLWithString(url), text]);
 }
